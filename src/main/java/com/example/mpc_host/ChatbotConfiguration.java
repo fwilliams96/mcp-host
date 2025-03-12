5a8096380d553b7138127ad73befdde84f5e2559
Francisco <fwmcomputer@gmail.com>
2025-03-12 01:01:00 +0100

    Use Docker instead of npx for github mcp server

--- src/main/java/com/example/mpc_host/ChatbotConfiguration.java
+++ src/main/java/com/example/mpc_host/ChatbotConfiguration.java
@@ -3,17 +3,25 @@ package com.example.mpc_host;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.mcp.SyncMcpToolCallbackProvider;
+import org.springframework.ai.tool.ToolCallbackProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 class ChatbotConfiguration {
 
+//    @Bean
+//    ChatClient chatClient(ChatModel chatModel,   toolCallbackProvider) {
+//        return ChatClient
+//                .builder(chatModel)
+//                .defaultTools(toolCallbackProvider.getToolCallbacks())
+//                .build();
+//    }
+
     @Bean
-    ChatClient chatClient(ChatModel chatModel, SyncMcpToolCallbackProvider toolCallbackProvider) {
-        return ChatClient
-                .builder(chatModel)
-                .defaultTools(toolCallbackProvider.getToolCallbacks())
+    public ChatClient chatClient(ChatClient.Builder chatClientBuilder, ToolCallbackProvider tools) {
+        return chatClientBuilder
+                .defaultTools(tools)
                 .build();
     }
 
